2bbc4d92441fc04637fdbe0cf200de9af1e028bd
guiqwer <guilherme.souza0996@gmail.com>
2024-11-12 09:19:52 -0300

    herança múltipla com default methods

---
+++ Defaultt Methods/src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import services.InterestService;
+import services.UsaInterestService;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Amount: ");
+        double amount = sc.nextDouble();
+        System.out.print("Months: ");
+        int months = sc.nextInt();
+
+        InterestService is = new UsaInterestService(1.0);
+        double payment = is.payment(amount, months);
+
+        System.out.println("Payment after"  + months + " months: ");
+        System.out.println(payment);
+
+
+    }
+}
